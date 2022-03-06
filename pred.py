2becc51e30cc296d323b91bc9917c88f6490ca32
git-thedarklord <shashanku943@gmail.com>
2022-03-06 10:25:53 +0530

    last

--- pred.py
+++ pred.py
@@ -1,4 +1,20 @@
 import pickle
-from sklearn import model_selection
+#from sklearn import model_selection
+import pandas as pd
+import warnings
+warnings.filterwarnings("ignore")
 model=pickle.load(open("SVM_trained_model","rb"))
-print(1)
+data=pd.read_csv("Train.csv")
+data.drop(["Unnamed: 0"],inplace=True,axis=1)
+d=data.head(1)
+def prediction(speed,rot,course):
+    d["speed"]=speed
+    d["course"]=course
+    d["rot"]=rot
+    
+    return model.predict(d)
+a=prediction(51.2,292,10)
+if(a==1):
+    print("YES, There is Risk")
+else:
+    print("NO")
